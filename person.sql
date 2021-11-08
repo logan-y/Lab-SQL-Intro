dca41cd6bda6b7056aa994b6b466daa6e63886ec
Logan Young <logany@att.net>
2021-11-08 14:35:01 -0600

    sql-intro lab

---
+++ person.sql
@@ -0,0 +1,54 @@
+-- Create a table called person that records a person’s id, name, age, height (in cm , city, favorite_color. id should be an auto-incrementing id/primary key (use type: SERIAL)
+CREATE TABLE person(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    height float,
+    city VARCHAR(40),
+    favorite_color VARCHAR(15)
+);
+
+-- Add 5 different people into the person database. Remember to not include the person_id because it should auto-increment.
+INSERT INTO person (name, age, height, city, favorite_color)
+VALUES ('Trew', 1000, 2000, 'Oakland', 'Tea'), 
+('Marcus', 3000, 1500, 'Vegas', 'Baby'), 
+('Andrew', 1000, 1750, 'Utah', 'Baby'), 
+('Cameron', 2500, 5000, 'Headquarters', 'Cigars'), 
+('Logan', 1000, 9000, 'Lafayette', 'Keyboard');
+
+-- Select all the people in the person table by height from tallest to shortest.
+SELECT height FROM person ORDER BY height DESC;
+
+-- Select all the people in the person table by height from shortest to tallest.
+SELECT height FROM person ORDER BY height ASC;
+
+-- Select all the people in the person table by age from oldest to youngest.
+SELECT age FROM person ORDER BY age DESC;SELECT age FROM person ORDER BY age DESC;
+
+-- Select all the people in the person table older than age 20.
+SELECT age FROM person WHERE age > 20;
+
+-- Select all the people in the person table that are exactly 18.
+SELECT age FROM person WHERE age = 18;
+
+-- Select all the people in the person table that are less than 20 and older than 30.
+
+
+-- Select all the people in the person table that are not 27 (use not equals).
+
+
+-- Select all the people in the person table where their favorite color is not red.
+
+
+-- Select all the people in the person table where their favorite color is not red and is not blue.
+
+
+-- Select all the people in the person table where their favorite color is orange or green.
+
+
+-- Select all the people in the person table where their favorite color is orange, green or blue (use IN).
+
+
+-- Select all the people in the person table where their favorite color is yellow or purple (use IN).
+
+
